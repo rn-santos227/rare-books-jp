50732c7e36e18ae71ade5b104ce684c82a4ae2c4
rn-santos227 <rn_santos227@yahoo.com>
2025-12-13 18:27:17 +0800

    updated sanity client library file

--- apps/web/lib/sanity.client.ts
+++ apps/web/lib/sanity.client.ts
@@ -1,8 +1,19 @@
 import { createClient } from '@sanity/client'
 
-export const sanityClient = createClient({
+const baseConfig = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: '2024-01-01',
+}
+
+export const sanityClient = createClient({
+  ...baseConfig,
   useCdn: true,
 })
+
+export const sanityWriteClient = createClient({
+  ...baseConfig,
+  token: process.env.SANITY_API_WRITE_TOKEN,
+  useCdn: false,
+})
+
