5c9447e34460fc93d9a4f5a247b3462f2dcfa297
rn-santos227 <rn_santos227@yahoo.com>
2025-12-13 15:57:30 +0800

    created book schema on sanity studio folder

---
+++ apps/studio/schemaTypes/book.ts
@@ -0,0 +1,88 @@
+import { defineType, defineField } from 'sanity'
+
+export default defineType({
+  name: 'book',
+  title: 'Book',
+  type: 'document',
+
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: Rule => Rule.required(),
+    }),
+
+    defineField({
+      name: 'author',
+      title: 'Author',
+      type: 'string',
+      validation: Rule => Rule.required(),
+    }),
+
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: Rule => Rule.required(),
+    }),
+
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+
+    defineField({
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    }),
+
+    defineField({
+      name: 'condition',
+      title: 'Condition',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'New', value: 'new' },
+          { title: 'Like New', value: 'like_new' },
+          { title: 'Used', value: 'used' },
+          { title: 'Damaged', value: 'damaged' },
+        ],
+      },
+    }),
+
+    defineField({
+      name: 'images',
+      title: 'Images',
+      type: 'array',
+      of: [{ type: 'image' }],
+    }),
+
+    defineField({
+      name: 'featured',
+      title: 'Featured',
+      type: 'boolean',
+      initialValue: false,
+    }),
+
+    defineField({
+      name: 'status',
+      title: 'Status',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Draft', value: 'draft' },
+          { title: 'Published', value: 'published' },
+          { title: 'Archived', value: 'archived' },
+        ],
+      },
+      initialValue: 'draft',
+    }),
+  ],
+})
