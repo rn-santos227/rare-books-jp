9afc1cfbe1d8a346fbd9987052f64c144e90bfd6
rn-santos227 <rn_santos227@yahoo.com>
2025-12-13 18:52:57 +0800

    updated sanity config file

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
+import {deskStructure} from './deskStructure'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: 'h53l3a36',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool({structure: deskStructure}), visionTool()],
 
   schema: {
     types: schemaTypes,
