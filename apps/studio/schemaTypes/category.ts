167c0a8019fd357c8ddc62a4ba13db2b0e42a69d
rn-santos227 <rn_santos227@yahoo.com>
2025-12-13 15:57:56 +0800

    created category schema on sanity studio folder

---
+++ apps/studio/schemaTypes/category.ts
@@ -0,0 +1,26 @@
+import { defineType, defineField } from 'sanity'
+
+export default defineType({
+  name: 'category',
+  title: 'Category',
+  type: 'document',
+
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: Rule => Rule.required(),
+    }),
+
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+      },
+      validation: Rule => Rule.required(),
+    }),
+  ],
+})
