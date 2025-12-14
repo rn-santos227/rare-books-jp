1a21f5ac7073c8b50fa797f78248c97691219cf7
rn-santos227 <rn_santos227@yahoo.com>
2025-12-14 19:16:00 +0800

    created filter panel component file in home

---
+++ apps/web/components/home/FilterPanel.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { Badge } from "@/components/ui/Badge";
+import { Button } from "@/components/ui/Button";
+import { TextField } from "@/components/ui/TextArea";
+import { FiltersState } from "@/hooks/useFilters";
+import { Category } from "@/types/category";
+import { Genre } from "@/types/genre";
+import { FilterPill } from "./FilterPill";
+
