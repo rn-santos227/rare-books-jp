56e5057162dfc817990acf4391997cd760d74bf2
rn-santos227 <rn_santos227@yahoo.com>
2025-12-14 20:00:36 +0800

    created promotion hero component in home

---
+++ apps/web/components/home/PromotionHero.tsx
@@ -0,0 +1,12 @@
+import { Badge } from "@/components/ui/Badge";
+import { Button } from "@/components/ui/Button";
+import ImageViewer from "@/components/ui/ImageViewer";
+import { Promotion } from "@/types/promotion";
+
+interface PromotionHeroProps {
+  categoriesCount: number;
+  genresCount: number;
+  promotion?: Promotion;
+}
+
+
