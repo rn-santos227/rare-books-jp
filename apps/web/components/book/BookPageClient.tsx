6e8a1934ebc6c6296945d3bbe356328e97593d1e
rn-santos227 <rn_santos227@yahoo.com>
2025-12-15 16:42:05 +0800

    created book page client component file in book

---
+++ apps/web/components/book/BookPageClient.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Link from "next/link";
+
+import { BookReviewSection } from "@/components/book/BookReviewSection";
+import { GeneralHeaderLayout } from "@/components/layouts/GeneralHeaderLayout";
+import { PageLayout } from "@/components/layouts/PageLayout";
+import { SiteFooter } from "@/components/layouts/SiteFooter";
+import { Badge } from "@/components/ui/Badge";
+import { Button } from "@/components/ui/Button";
+import ImageViewer from "@/components/ui/ImageViewer";
+import { Book } from "@/types/book";
+import { Review } from "@/types/review";
