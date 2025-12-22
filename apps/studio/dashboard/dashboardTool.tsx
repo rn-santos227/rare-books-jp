b7f58d9c26f9f8aa77e78cfe07b77c0a6978f670
rn-santos227 <rn_santos227@yahoo.com>
2025-12-22 18:27:16 +0800

    updated dashboard tool file in sanity studio folder

--- apps/studio/dashboard/dashboardTool.tsx
+++ apps/studio/dashboard/dashboardTool.tsx
@@ -20,4 +20,26 @@ import {
 import {useEffect, useMemo, useState} from 'react'
 import {definePlugin, useClient, useWorkspace} from 'sanity'
 
+type RecentDocument = {
+  _id: string
+  _type: string
+  _updatedAt: string
+  title?: string
+  name?: string
+  slug?: {current?: string}
+}
+
+const RECENT_DOCUMENTS_QUERY = `
+  *[
+    _type != "sanity.imageAsset" &&
+    _type != "sanity.fileAsset"
+  ] | order(_updatedAt desc)[0...6]{
+    _id,
+    _type,
+    _updatedAt,
+    title,
+    name,
+    slug
+  }
+`
 
