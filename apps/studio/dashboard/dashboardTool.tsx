595545913dbe9739755b69b32283c872681d6bca
rn-santos227 <rn_santos227@yahoo.com>
2025-12-22 18:26:59 +0800

    created dashboard tool file in sanity studio folder

---
+++ apps/studio/dashboard/dashboardTool.tsx
@@ -0,0 +1,23 @@
+import {
+  BookIcon,
+  ComposeIcon,
+  DocumentIcon,
+  HelpCircleIcon,
+  HomeIcon,
+} from '@sanity/icons'
+import {
+  Badge,
+  Box,
+  Button,
+  Card,
+  Flex,
+  Grid,
+  Heading,
+  Spinner,
+  Stack,
+  Text,
+} from '@sanity/ui'
+import {useEffect, useMemo, useState} from 'react'
+import {definePlugin, useClient, useWorkspace} from 'sanity'
+
+
