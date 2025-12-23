90325ab5020f27cce0ff7fad9bc5254006803bf6
rn-santos227 <rn_santos227@yahoo.com>
2025-12-23 19:59:17 +0800

    created filter drop down component file

---
+++ apps/web/components/catalog/FilterDropdown.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Badge } from "@/components/ui";
+import { SupportedLanguage } from "@/constants/translations";
+import { getLocalizedText } from "@/lib/localization";
+
+type LocalizedEntity = {
+  _id: string;
+  name: string;
+  nameJa?: string | null;
+};
+
+type FilterDropdownProps = {
+  label: string;
+  placeholder: string;
+  items: LocalizedEntity[];
+  selected: string[];
+  language: SupportedLanguage;
+  helper: string;
+  badgeTone: "info" | "neutral";
+  clearLabel: string;
+  resetLabel: string;
+  onChange: (ids: string[]) => void;
+  onClear: () => void;
+};
+
+
+
